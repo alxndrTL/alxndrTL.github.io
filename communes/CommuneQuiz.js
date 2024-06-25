b3b173f6c88b5a2e2aea9aace97f5b55efbcc26f
Alexandre TL <alextorresleguet@icloud.com>
2024-06-25 17:39:38 +0200

    Update CommuneQuiz.js

--- communes/CommuneQuiz.js
+++ communes/CommuneQuiz.js
@@ -57,8 +57,6 @@ const CommuneQuiz = () => {
     return <div>Chargement...</div>;
   }
 
-
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <Card className="w-full max-w-md">
