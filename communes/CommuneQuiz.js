bdfe5bef51f7b32b31fbd7a19b7c95b4ec9c9d93
Alexandre TL <alextorresleguet@icloud.com>
2024-06-25 18:28:25 +0200

    Update CommuneQuiz.js

--- communes/CommuneQuiz.js
+++ communes/CommuneQuiz.js
@@ -13,8 +13,8 @@ const CommuneQuiz = () => {
 
   const fetchCommunes = async () => {
     const [vraiesResponse, faussesResponse] = await Promise.all([
-      fetch('/data/vraiesCommunes.txt'),
-      fetch('/data/faussesCommunes.txt'),
+      fetch('communes/data/vraiesCommunes.txt'),
+      fetch('communes/data/faussesCommunes.txt'),
     ]);
 
     const vraiesText = await vraiesResponse.text();
